4c091bd290da1c114c7f004d5a4d2adec2a37715
Rodaaq69 <122701094+Rodaaq69@users.noreply.github.com>
2024-04-22 17:12:57 -0600

    Add files via upload

---
+++ Repositorio EVA3/EVA3_1/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int arreglo [] = new int[10];
+        arreglo[1]=100;
+        System.out.println("Valor de arreglo[1]  ="+arreglo[1] );
+        System.out.println(arreglo.length);
+    }
+}
