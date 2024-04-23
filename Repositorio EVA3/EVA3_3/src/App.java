e3fd591a860714c9b396e9c440bc8988e4a41cab
Rodaaq69 <122701094+Rodaaq69@users.noreply.github.com>
2024-04-23 16:52:41 -0600

    Add files via upload

--- Repositorio EVA3/EVA3_3/src/App.java
+++ Repositorio EVA3/EVA3_3/src/App.java
@@ -1,28 +1,7 @@
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner read = new Scanner(System.in);
-        System.out.println("Introduce el texto");
-        String text=read.nextLine();
-        System.out.println("Cual es el numero a calcular la sumatoria");
-        int cant=read.nextInt();
-        int sumatoria=0;
-        for(int i=0; i<=cant; i++){
-            sumatoria=sumatoria+i;
-        }
-        System.out.println("El valor de la sumatoria de "+cant+" = "+sumatoria);
-
-        System.out.println("Ingrese la cantidad de filas");
-        int filas=read.nextInt();
-        for(int i=1; i<=filas; i++){
-            for(int j=i; j>=1; j--){
-                System.out.print("*");
-            }
-            System.out.println(" ");
-        }
+        int datos[]= new int[10];
+        datos[10]=100;
+            System.err.println("el valor de datos[10] = "+ datos[10]);
     }
-
-    
-
 }
